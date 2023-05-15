8e2105eb2548fae0c870786b5772177dea3e366a
forsigner <forsigner@gmail.com>
2023-05-15 10:18:26 +0800

    feat: can toast

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,25 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
 import { View, Text } from '@fower/react-native'
 import { SafeAreaView } from 'react-native'
+import { RootSiblingParent } from 'react-native-root-siblings'
+
 import { Nav } from './components/Nav'
 import { TranslatorInput } from './components/TranslatorInput'
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View column flex-1 toCenter>
-        <StatusBar style="auto" />
-        <Nav />
-        <TranslatorInput />
-        <View flex-1>
-          <Text>H Open up App.tsx to start working on your app!</Text>
-          <View>
-            <Text p0 red300>
-              Hello
-            </Text>
+    <RootSiblingParent>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View column flex-1 toCenter>
+          <StatusBar style="auto" />
+          <Nav />
+          <TranslatorInput />
+          <View flex-1>
+            <View>
+              <Text p0 red300>
+                TODO...
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </RootSiblingParent>
   )
 }
--- apps/mobile/components/TranslatorInput.tsx
+++ apps/mobile/components/TranslatorInput.tsx
@@ -1,15 +1,30 @@
 import { View, Text, TextInput } from '@fower/react-native'
 import { Button, Colors } from 'react-native-ui-lib'
-import { IconCopy } from './IconCopy'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { useState } from 'react'
+import Toast from 'react-native-root-toast'
 
 export function TranslatorInput() {
+  const [value, setValue] = useState('')
   return (
     <View w-100p p3>
       <View w-100p border-2 borderBlack roundedXL>
-        <TextInput rounded2XL px3 py3 minH-120 maxH-200 multiline placeholder="Enter text" />
-        <View row toBetween px1 pb1>
+        <TextInput
+          rounded2XL
+          px3
+          py3
+          minH-120
+          maxH-200
+          multiline
+          placeholder="Enter text"
+          value={value}
+          onChangeText={(text) => {
+            setValue(text)
+          }}
+        />
+        <View row toBetween toCenterY px1 pb1>
           <View>
-            <IconCopy />
+            <Ionicons name="md-copy-outline" size={24} color="black" />
           </View>
           <View>
             <Button
@@ -17,6 +32,10 @@ export function TranslatorInput() {
               size={Button.sizes.medium}
               backgroundColor={Colors.black}
               borderRadius={8}
+              onPress={() => {
+                console.log('value:', value)
+                Toast.show('Translated')
+              }}
             />
           </View>
         </View>
