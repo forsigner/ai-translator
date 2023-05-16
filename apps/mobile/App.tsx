833ef1c4c9e1b78396e048c59ed8328edc1eda50
forsigner <forsigner@gmail.com>
2023-05-16 19:00:56 +0800

    feat: use react-native-gifted-chat

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar'
 import { View, Text } from '@fower/react-native'
-import { SafeAreaView } from 'react-native'
+import { Platform, SafeAreaView, StatusBar as RNStatusBar } from 'react-native'
 import { RootSiblingParent } from 'react-native-root-siblings'
 import { BotProvider } from '@ai-translator/bot'
 import { Nav } from './components/Nav'
 import { TranslatorInput } from './components/TranslatorInput'
 import { TranslatorContent } from './components/TranslatorContent'
+import { Chat } from './components/Chat'
 
 export default function App() {
   return (
     <RootSiblingParent>
       <BotProvider>
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView
+          style={{ flex: 1, paddingTop: Platform.OS === 'android' ? RNStatusBar.currentHeight : 0 }}
+        >
           <View column flex-1 toCenter>
             <StatusBar style="auto" />
             <Nav />
@@ -20,6 +23,7 @@ export default function App() {
               <TranslatorContent />
             </View>
           </View>
+          <Chat></Chat>
         </SafeAreaView>
       </BotProvider>
     </RootSiblingParent>
